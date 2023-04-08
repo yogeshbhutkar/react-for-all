64d00d0354bec630ff79006313da3412c3b61e81
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-04-08 14:51:24 +0530

    Added the input tags date of birth and phone number

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppContainer from "./AppContainer";
+import Header from "./Header";
+
+const formFields = [
+  { id: 1, label: "First Name", type: "text" },
+  { id: 2, label: "Last Name", type: "text" },
+  { id: 3, label: "Email", type: "email" },
+  { id: 4, label: "Date of Birth", type: "date" },
+  { id: 5, label: "Phone Number", type: "number" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
+        <Header
+          title={`welcome to Lesson 5 of $react-typescript with #tailwindcss`}
+        />
+        {formFields.map((field) => (
+          <div key={field.id}>
+            <label>{field.label}</label>
+            <input
+              className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
+              type={field.type}
+            />
+          </div>
+        ))}
+        <button className="bg-blue-500 text-white px-5 py-2 rounded-xl mt-2 font-semibold">
+          Submit
+        </button>
+      </div>
+    </AppContainer>
   );
 }
 
