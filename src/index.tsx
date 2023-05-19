3dea723252751c5b9f5504c86e71d522c706f8b6
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-05-19 09:49:59 +0530

    Added production services and sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import * as Sentry from "@sentry/react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://d4c1e8c482034f6eacfba60c0d145a22@o4505208628379648.ingest.sentry.io/4505208632246272",
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
